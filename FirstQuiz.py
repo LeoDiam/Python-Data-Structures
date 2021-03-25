1fbfacd47f3fbbbb3f0eea71d072dabc9ad8b960
LeoDiam <t8190243@aueb.gr>
2021-03-25 15:52:03 +0200

    easy exercise of the edx course

---
+++ FirstQuiz.py
@@ -0,0 +1,8 @@
+x = 'From marquard@uct.ac.za'
+print(x[14:17])
+x = 'Bob'
+print(x.upper())
+data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+pos = data.find('.')
+print(pos)
+print(data[pos:pos+3])
